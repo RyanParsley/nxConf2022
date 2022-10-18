f5c0933bc2b45c1290c34737e8d9547a37cb2aa4
Ryan Parsley <ryan.parsley@jbhunt.com>
2022-10-18 15:58:24 -0400

    chore: fix migrated tests

--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GameDetailComponent } from './game-detail.component';
 
@@ -8,6 +10,10 @@ describe('GameDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule
+      ],
       declarations: [GameDetailComponent],
     }).compileComponents();
 
